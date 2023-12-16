ffaaded319857537ca3eb679ebfb9e0649854872
Rfusar <rfusaro12@gmail.com>
2023-12-16 06:04:43 +0100

    0.0.0

--- creazione_immagine/inizializzazioneImmagine.sh
+++ creazione_immagine/inizializzazioneImmagine.sh
@@ -1,22 +1,50 @@
 #!/bin/bash
-cd ..
 
-firefox https://hub.docker.com
-read
+#NEL CASO CANCELLI L'IMMAGINE
+var1=true
+while $var; do
+    clear
+    read -p "Devi gestire le immagini da  dockerHUB? [Y/N] " risposta
+    if [ "$risposta" = "Y" ]; then
+    	firefox https://hub.docker.com
+	read
+	var=false
+    elif [ "$risposta" = "N" ]; then
+	var=false 
+    fi
+done
 
 clear
 git clone git@github.com:Rfusar/dashboard.git
 
-#ELIMINA
+
+#DECISIONE DEL BRANCH
+branch=true
+while $branch; do
+    clear
+    read -p "Quale branch vuoi utilizzare? " risposta
+    if [ "$risposta" = "DashboardVersatile" ]; then
+	branch=false
+    elif [ "$risposta" = "Repository_GDPR" ]; then
+	cd dashboard 
+	git checkout Repository_GDPR
+	git branch -d DashboardVersatile
+	cd ..
+	branch=false 
+    fi
+done
+
+
+#PREPARAZIONE ALL'IMMAGINE
 cp -r dashboard dashboard_immagine
 cd dashboard_immagine
 rm .env
 rm -rdf .git
 cd ..
 
-#COPIA
 cp .env dashboard_immagine
 cp Dockerfile dashboard_immagine
+
 
 #CREAZIONE IMMAGINE
 sudo docker login
@@ -24,13 +52,14 @@ cd dashboard_immagine
 sudo docker build -t rfusaro12276/web_page:latest .
 cd ..
 
+
 #TEST
 sudo docker start psql
 sudo docker run --name web_page rfusaro12276/web_page:latest
 
-#FASE PULIZIA
+
+#ELIMINAZIONE DELLE CARTELLE
 clear
-sudo docker rm web_page
 sudo docker stop psql
 rm -rfd dashboard
 rm -rfd dashboard_immagine
